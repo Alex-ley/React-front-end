7ed7ad42f3bf89c338e64d7df5b1685ebb78fef4
Alexander Ley <91.alex.ley@googlemail.com>
2019-04-08 17:23:15 +0200

    added Materialize sidenav, changed Nav to Class based component

--- gym-ai/src/components/Navbar.js
+++ gym-ai/src/components/Navbar.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { Link, NavLink, withRouter } from 'react-router-dom'
 import { Link, NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signOut } from '../store/actions/authActions'
+import M from "materialize-css/dist/js/materialize.min.js";
 
-const Navbar = (props) => {
-  const { auth, profile, signOut } = props;
-  // console.log(auth);
-  const links = auth.uid ? <SignedInLinks signOut={signOut} profile={profile} /> : <SignedOutLinks />;
+class Navbar extends Component {
 
-  return (
-    <nav className="nav-wrapper light-blue darken-4" id="nav_wrapper">
-      <div className="container">
-        <Link className="brand-logo" id="nav_logo" to="/"><i className="material-icons">fitness_center</i>Fitness.ai</Link>
-        {links}
+  render(){
+    const { auth, profile, signOut } = this.props;
+    // console.log(auth);
+    const links = auth.uid ? <SignedInLinks signOut={signOut} profile={profile} className={"right hide-on-med-and-down"}/> : <SignedOutLinks className={"right hide-on-med-and-down"}/>;
+    const mobile_links = auth.uid ? <SignedInLinks signOut={signOut} profile={profile} className={"sidenav"}/> : <SignedOutLinks className={"sidenav"}/>;
+    return (
+      <div>
+      <nav className="nav-wrapper light-blue darken-4" id="nav_wrapper">
+        <div className="container">
+          <Link className="brand-logo" id="nav_logo" to="/"><i className="material-icons">fitness_center</i>Fitness.ai</Link>
+          <NavLink to="#" data-target="mobile-sidenav" className="sidenav-trigger"><i className="material-icons">menu</i></NavLink>
+          {links}
+        </div>
+      </nav>
+        {mobile_links}
       </div>
-    </nav>
-  )
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
@@ -33,29 +41,54 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const SignedInLinks = (props) => {
-  return (
-    <div>
-      <ul className="right">
-        <li><NavLink to='/add'>Add Content</NavLink></li>
-        <li><button className="link-button" onClick={props.signOut}>Log Out</button></li>
-        <li><NavLink to='/' className="btn btn-floating teal lighten-1">
-          {props.profile.initials}
-        </NavLink></li>
-      </ul>
-    </div>
-  )
+class SignedInLinks extends Component {
+  componentDidMount() {
+        const elem = document.querySelector(".sidenav");
+        M.Sidenav.init(elem, {
+            edge: "left",
+            inDuration: 250
+        });
+    }
+  render(){
+    const { className } = this.props;
+    const id = className === "sidenav" ? "mobile-sidenav" : "main-nav";
+    const a_class = className === "sidenav" ? "sidenav-close " : "";
+    const a_button = className === "sidenav" ? "sidenav-button" : "nav-button";
+    return (
+      <div>
+        <ul className={className} id={id}>
+          <li><NavLink className={a_class} to='/add'>Add Content</NavLink></li>
+          <li><NavLink to="#" className={a_class} onClick={this.props.signOut}>Log Out</NavLink></li>
+          <li><NavLink to='/' id={a_button} className={a_class + "btn btn-floating teal lighten-1"}>
+            {this.props.profile.initials}
+          </NavLink></li>
+        </ul>
+      </div>
+    )
+  }
 }
 
-const SignedOutLinks = () => {
-  return (
-    <div>
-      <ul className="right">
-        <li><NavLink to='/signup'>Signup</NavLink></li>
-        <li><NavLink to='/signin'>Login</NavLink></li>
-      </ul>
-    </div>
-  )
+class SignedOutLinks extends Component {
+  componentDidMount() {
+        const elem = document.querySelector(".sidenav");
+        M.Sidenav.init(elem, {
+            edge: "left",
+            inDuration: 250
+        });
+    }
+  render(){
+    const { className } = this.props;
+    const id = className === "sidenav" ? "mobile-sidenav" : "main-nav";
+    const a_class = className === "sidenav" ? "sidenav-close " : "";
+    return (
+      <div>
+        <ul className={className} id={id}>
+          <li><NavLink to='/signup' className={a_class}>Signup</NavLink></li>
+          <li><NavLink to='/signin' className={a_class}>Login</NavLink></li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
